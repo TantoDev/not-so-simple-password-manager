8929eeee8dfb41c694023aad1a32f4801caeeba1
Tanto <156586407+TantoDev@users.noreply.github.com>
2025-06-05 22:27:15 +0100

    Added master password system, clear-all option, and input validation
    Implemented a master password system to restrict access to the password manager. Also added a 'clear all' option for deleting stored credentials, and tweaked the input prompts to reject empty responses.
    
    Honestly, I tried to do more than I was ready for, and the code turned out messy but it works, which honestly surprises me. I’ll rework this in a cleaner version later, but for now, I’m putting it out as-is.

--- password_manger.py
+++ password_manger.py
@@ -1,3 +1,4 @@
+
 import json
 
 class Credential:
@@ -18,13 +19,26 @@ class Credential:
 class Vault:
 	def __init__(self):
 		self.vault = []
+		self.mastervault = {}
 		self.filename = 'vault.json'
+		self.masterfile = 'mastervault.json'
 		self.loadfile()
+		self.loadlock()
+
+# prevent empty response from user	
+	def safe_input(self,prompt, var_name="This field"):
+		field = input(prompt).strip()
+		while True:
+			if not field:
+				print(f"\n {var_name} cannot be empty!")
+				field = input(prompt).strip()
+			else:
+				return field
 		
 	def addCredential(self):
-		title = input(" Enter the title: ")
-		username = input(" Enter your username: ")
-		password = input(" Enter your password: ")
+		title = self.safe_input(" Enter the title: ")
+		username = self.safe_input(" Enter your username: ")
+		password = self.safe_input(" Enter your password: ")
 		myCred = Credential(title,username, password)
 		self.vault.append(myCred)
 		self.savefile()
@@ -44,7 +58,7 @@ class Vault:
 			print("\n Your vault is empty!")
 		else:
 			found = False
-			search_term = input("\n Enter a search term: ")
+			search_term = self.safe_input("\n Enter a search term: ")
 			for cred in self.vault:
 				if search_term.lower() in cred.title.lower() or search_term.lower() in cred.name:
 					print(f" {cred.title} \n Username: {cred.name} \n Password: {cred.password}")
@@ -58,7 +72,7 @@ class Vault:
 		if not self.vault:
 			print("\n Your vault is empty!")
 		else:
-			word = input("\n Enter the title of the credential you wish to delete: ")
+			word = self.safe_input("\n Enter the title of the credential you wish to delete: ")
 			for cred in self.vault:
 				if word.lower() == cred.title.lower():
 					self.vault.remove(cred)
@@ -68,11 +82,31 @@ class Vault:
 			if not found:
 				print("\n The entry doesn't exist in your vault")
 
+# clears vault	
+	def clear(self):
+		if not self.vault:
+			print("\n Your vault is empty!")
+		else:
+			reply = input("\n Are you sure you want to clear your vault? Y/N: ").lower().strip()
+			if reply in ['yes','y']:
+				self.vault.clear()
+				self.savefile()
+				print("\n Vault has been cleared!")
+			elif reply in ['no','n']:
+				print("\n Phew! Vault still intact")
+			else:
+				print('\n Invalid response! Please type Y or N')
+
 # saves user data				
 	def savefile(self):
 		data = [(cred.to_dict()) for cred in self.vault]
 		with open(self.filename,'w') as f:
 			json.dump(data,f)
+
+# saves the master password
+	def savelock(self):
+		with open(self.masterfile, 'w') as f:
+			json.dump(self.mastervault,f)
 
 # retrives saved data			
 	def loadfile(self):
@@ -80,40 +114,88 @@ class Vault:
 			with open(self.filename,'r') as f:
 				data = json.load(f)
 				self.vault = [(Credential.from_dict(item)) for item in data]
-		except FileNotFoundError:
+		except (FileNotFoundError, json.JSONDecodeError, TypeError):
 			self.vault = []
-		except json.JSONDecodeError:
-			self.vault = []
-		except TypeError:
-			self.vault = []
+
+# retrieves master password for authentication	
+	def loadlock(self):
+		try:
+			with open(self.masterfile,'r') as f:
+				self.mastervault = json.load(f)
+		except (FileNotFoundError, json.JSONDecodeError, TypeError):
+			self.mastervault = {}
 			
-myVault = Vault()	
-menu_text = '''
+	def display_menu(self):
+		menu_text = '''
  Simple Password Manager~©
  1. Add Credential
  2. View Vault
  3. Search
  4. Delete Credential
- 5. Exit
+ 5. Clear Vault
+ 6. Exit
 '''
 # menu interface
-while True:
-	try:
-		response = input(menu_text+"\n Select your choice: ").strip()
-		if response == '1':
-			myVault.addCredential()
-		elif response == '2':
-			myVault.viewAll()
-		elif response == '3':
-			myVault.search()
-		elif response == '4':
-			myVault.delete()
-		elif response == '5':
-			print("\nHave a nice day!\n")
-			break
-		else:
-			print("\nInvalid input!")						
-	except ValueError:
-		print("Enter a numerical value")
 
-    
+		while True:
+			try:
+				response = input(menu_text+"\n Select your choice: ").strip()
+				if response == '1':
+					myVault.addCredential()
+				elif response == '2':
+					myVault.viewAll()
+				elif response == '3':
+					myVault.search()
+				elif response == '4':
+					myVault.delete()
+				elif response == '5':
+					myVault.clear()
+				elif response == '6':
+					print("\n  Have a nice day!\n")
+					break
+				else:
+					print("\nInvalid input!")						
+			except ValueError:
+				print("Enter a numerical value")
+			
+	def register(self):
+		self.mastervault['name'] = self.safe_input("\n Enter your name: ")
+		self.savelock()
+		print(f"\n Hi {self.mastervault['name']}, \n\n To start using your password manager you need to first create your master password.\n\n It would be required every time you try to access your vault.")
+		while True:
+			password = self.safe_input("\n Enter your Master Password: ")
+			self.mastervault['password'] = self.safe_input("\n Confirm Password: ")
+			if password == self.mastervault['password']:
+				self.savelock()
+				print("\n Password set successfully!")						
+				return True	
+			else:
+				print("\n Passwords dont match! Try again.")
+			return False
+
+	def resume(self):	
+		while True:
+			reply = self.safe_input("\n To access your Password Vault\n Enter master password: ")
+			if len(self.mastervault) == 2:
+						
+				if reply == self.mastervault['password']:
+					print("\n Verified!")
+					self.display_menu()
+					return True
+				else:
+					print(f"\n Wrong password! Try again: ")
+			else:
+				print("\n Oops! seems like you haven't yet created your master password.\n No worries, try creating one now.")
+				self.register()
+							
+	def lock(self):
+		if 'name' not in self.mastervault:
+			  self.register()
+		print(f"\n Welcome back {self.mastervault['name']}!" )
+		self.resume()
+							
+myVault = Vault()
+myVault.lock()	
+
+
+	
